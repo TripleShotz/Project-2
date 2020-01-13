394df92f297537f1d057f4e4213221670bbfbc72
Fabién Kouwenberg <fabienko266@gmail.com>
2020-01-13 18:20:08 +0100

    no message

---
+++ Assets/Scripts/WebTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebTest : MonoBehaviour
+{
+    // use this for initalization
+    IEnumerator Start()
+    {
+        WWW request = new WWW("http://localhost/PRO_2/sqlconnect/webtest.php");
+        yield return request;
+        string[] webResults = request.text.Split('\t');
+        Debug.Log(webResults[0]);
+        int webNumber = int.Parse(webResults[1]);
+        webNumber *=2;
+        Debug.Log(webNumber);
+    }
+
+
+}
